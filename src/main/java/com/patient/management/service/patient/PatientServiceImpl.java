1ae723167f243015ae85a4ace04857cc29d9b8b1
Akshura23 <40722099+Akshura23@users.noreply.github.com>
2025-03-24 02:39:53 +0530

    Date - 2025-03-23 Description - created PatientController

--- src/main/java/com/patient/management/service/patient/PatientServiceImpl.java
+++ src/main/java/com/patient/management/service/patient/PatientServiceImpl.java
@@ -1,7 +1,28 @@
 package com.patient.management.service.patient;
 
+import com.patient.management.entity.PatientEntity;
+import javafx.collections.ObservableList;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientServiceImpl implements PatientService {
+    @Override
+    public ObservableList<PatientEntity> getAllPatients() {
+        return null;
+    }
+
+    @Override
+    public void addPatient(PatientEntity patient) {
+
+    }
+
+    @Override
+    public void updatePatient(PatientEntity selectedPatient) {
+
+    }
+
+    @Override
+    public void deletePatient(PatientEntity selectedPatient) {
+
+    }
 }
